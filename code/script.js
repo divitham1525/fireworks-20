3fbffbd31d9255fe13ce2fffc2d458949e4e12f1
divitham1525 <117502827+divitham1525@users.noreply.github.com>
2023-12-05 12:35:05 +0530

    Add files via upload

---
+++ code/script.js
@@ -0,0 +1,12 @@
+// Add any interactive features or dynamic content using JavaScript
+// For example, you could toggle visibility of sections, implement a light/dark mode, etc.
+// For a basic setup, you can start with console logs or simple DOM manipulations
+console.log('Hello from script.js!');
+
+// You can add JavaScript code here to interact with your HTML content
+// For example, selecting elements and modifying their behavior or content
+// Example:
+// const skillsList = document.getElementById('skills');
+// skillsList.addEventListener('click', () => {
+//     console.log('Skills section clicked!');
+// });
